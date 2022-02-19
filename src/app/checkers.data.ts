32017f32e6eaa4a5fa8d097cf8940d7465c45b45
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-02-19 14:16:32 +0100

    Reafactor + Feat : Modification du noms des type et création d'une fonction de récupération du gamestate inisial

--- src/app/checkers.data.ts
+++ src/app/checkers.data.ts
@@ -1,21 +1,21 @@
 //Game turn
 import {Observable} from "rxjs";
 
-export type Turn = 'white' | 'black';
+export type Turn = 'White' | 'Black';
 //Different types of discs
-export type Discs = Turn | 'blackKing' | 'whiteKing';
+export type Discs = Turn | 'BlackKing' | 'WhiteKing';
 //The content of a cell
 export type Cell = 'Empty' | Discs;
 
 //Row of the board
-export type Row = [Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell];
+export type Row = [Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell,Cell,Cell];
 //The board
-export type Board = [Row, Row, Row, Row, Row, Row, Row, Row];
+export type Board = [Row, Row, Row, Row, Row, Row, Row, Row,Row,Row];
 
 //Immutable board row
-export type Row_RO = readonly [Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell];
+export type Row_RO = readonly [Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell,Cell,Cell];
 //Immutable board
-export type Board_RO = readonly [Row_RO, Row_RO, Row_RO, Row_RO, Row_RO, Row_RO, Row_RO, Row_RO];
+export type Board_RO = readonly [Row_RO, Row_RO, Row_RO, Row_RO, Row_RO, Row_RO, Row_RO, Row_RO,Row_RO,Row_RO];
 
 //One coordinate of the board
 export type TileCoords = readonly [x:number, y:number];
@@ -46,4 +46,47 @@ export interface GameStateInterface {
     playMove(from:TileCoords, to:TileCoords):playReturns;
     //Observable of the game state
     gameStateObjs:Observable<GameState>;
+}
+
+/**
+ * This function generate the initial game state
+ * - The board is a 10x10 array of cells
+ * - At the beginning of the game, the board looks like this :
+ * ```
+ * ·=Empty, B=Black, W=White
+ * +----------+
+ * |·B·B·B·B·B|
+ * |B·B·B·B·B·|
+ * |·B·B·B·B·B|
+ * |..........|
+ * |..........|
+ * |·W·W·W·W·W|
+ * |W·W·W·W·W·|
+ * |·W·W·W·W·W|
+ * +----------+
+ *```
+ * @return GameState
+ */
+export function getInitialGameState():GameState{
+  return {
+    board:new Array(10).fill(0).map((_,rowIndex)=>{
+      return new Array(10).fill('Empty').map((_,columnIndex)=>{
+        if(rowIndex%2==1 && columnIndex%2==1){
+          if(rowIndex<3){
+            return 'White'
+          }else if (rowIndex>4){
+            return 'Black'
+          }
+        }else if(rowIndex%2==0 && columnIndex%2==0){
+          if(rowIndex<3){
+            return 'White'
+          }else if (rowIndex>4){
+            return 'Black'
+          }
+        }
+        return 'Empty'
+      })
+    }) as Board,
+    turn:'White'
+  };
 }
