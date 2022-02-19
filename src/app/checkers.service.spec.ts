0c378da0089f93876a8127347901770c6a4df28d
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-02-19 16:27:02 +0100

    Test : Mise en place du squelette de test

--- src/app/checkers.service.spec.ts
+++ src/app/checkers.service.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CheckersService } from './checkers.service';
-import {Board_RO} from "./checkers.data";
 
 describe('CheckersService', () => {
   let service: CheckersService;
@@ -13,17 +12,81 @@ describe('CheckersService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
-    service.board = [
-      ['White','Empty','White','Empty','White','Empty','White','Empty','White','Empty'],
-      ['Empty','White','Empty','White','Empty','White','Empty','White','White','Empty'],
-      ['White','Empty','White','Empty','White','Empty','White','Empty','White','Empty'],
-      ['Empty','White','Empty','White','Empty','White','Empty','White','White','Empty'],
-      ['White','Empty','White','Empty','White','Empty','White','Empty','White','Empty'],
-      ['Empty','White','Empty','White','Empty','White','Empty','White','White','Empty'],
-      ['White','Empty','White','Empty','White','Empty','White','Empty','White','Empty'],
-      ['Empty','White','Empty','White','Empty','White','Empty','White','White','Empty'],
-      ['White','Empty','White','Empty','White','Empty','White','Empty','White','Empty'],
-      ['Empty','White','Empty','White','Empty','White','Empty','White','White','Empty'],
-    ]
+  });
+  /**
+   * Test of initialize(board: Board, turn:Turn): InitializeReturns
+   */
+  describe('Test initialize() Methode',()=>{
+    describe('Errors',()=>{
+      describe('Too many pieces',()=>{
+
+      });
+      describe('Invalid pieces placement',()=>{
+
+      });
+    });
+    describe('Will be initialize',()=>{
+
+    });
+  });
+
+
+  /**
+   * Test of whereCanPlay(from: TileCoords): WhereCanPlayReturns
+   */
+  describe('Test whereCanPlay() Methode',()=>{
+    describe('Errors',()=>{
+      describe('Out of range',()=>{
+
+      });
+      describe('No piece',()=>{
+
+      });
+    });
+    describe('Will be return the list of possible moves',()=>{
+
+    });
+  });
+
+
+  /**
+   * Test of playMove(from: TileCoords, to: TileCoords): PlayReturns
+   */
+  describe('Test playMove() Methode',()=>{
+    describe('Errors',()=>{
+      describe('Out of range',()=>{
+
+      });
+      describe('No piece',()=>{
+
+      });
+      describe('Not your turn',()=>{
+
+      });
+      describe('Can not move',()=>{
+
+      });
+    });
+    describe('Will be return play the move',()=>{
+
+    });
+  });
+
+  /**
+   * Test of winner(): Win
+   */
+  describe('Test winner() Methode',()=>{
+    describe('Will be return Black winner',()=>{
+
+    });
+    describe('Will be return White winner',()=>{
+
+    });
+    describe('Will be return Equality',()=>{
+
+    });
+    describe('Will be return no winner',()=>{
+
+    });
   });
 });
