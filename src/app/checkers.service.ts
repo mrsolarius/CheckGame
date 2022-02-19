f90dd676aa0e62b74ee2baaa823d3ffd02bded7e
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-02-19 14:17:13 +0100

    Feat : Implementation de GameStateInterface dans le services

--- src/app/checkers.service.ts
+++ src/app/checkers.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import {Board_RO, GameState, GameStateInterface, playReturns, TileCoords, TileCoordsList, Turn, getInitialGameState} from "./checkers.data";
+import {BehaviorSubject, Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
-export class CheckersService {
+export class CheckersService implements GameStateInterface{
 
-  constructor() { }
+  constructor() {
+    this.gameStateSubject.next(getInitialGameState());
+  }
+
+  protected gameStateSubject = new BehaviorSubject<GameState>(getInitialGameState());
+  gameStateObjs: Observable<GameState> = this.gameStateSubject.asObservable();
+
+  get turn(): Turn {
+    return this.gameStateSubject.value.turn;
+  }
+
+  get board(): Board_RO {
+    return this.gameStateSubject.value.board;
+  }
+
+  get winner(): Turn | null{
+    return null;
+  };
+
+  playMove(from: TileCoords, to: TileCoords): playReturns {
+    return null;
+  }
+
+  whereCanPlay(from: TileCoords): TileCoordsList {
+    return [];
+  }
 }
